777e66c7a0f4ee8ff3894afb0f5078773cd49678
jokergoo <jokergoo@gmail.com>
2015-02-24 22:33:32 +0100

    extrace ontology directly from html code

---
+++ R/00_S4_generic_methods.R
@@ -0,0 +1,8 @@
+
+setGeneric('plotRegionGeneAssociationGraphs', function(job, ...) standardGeneric('plotRegionGeneAssociationGraphs'))
+
+setGeneric('availableCategories', function(job, ...) standardGeneric('availableCategories'))
+
+setGeneric('getEnrichmentTables', function(job, ...) standardGeneric('getEnrichmentTables'))
+
+setGeneric('availableOntologies', function(job, ...) standardGeneric('availableOntologies'))
