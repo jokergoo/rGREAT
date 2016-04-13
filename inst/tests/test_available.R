aab32809e94b98dde75a5f458016376e2916eeb3
Zuguang Gu <jokergoo@gmail.com>
2016-04-13 20:50:03 +0200

    commit

--- inst/tests/test_available.R
+++ inst/tests/test_available.R
@@ -1,5 +1,14 @@
 context("Test someting")
 
+library(rGREAT)
+
+oe = try(httpHEAD(gsub("public/cgi-bin$", "", rGREAT:::BASE_URL_LIST[["default"]]), .opts = list(connecttimeout = 10)), quiet = TRUE)
+connected = TRUE
+if(inherits(oe, "try-error")) {
+	connected = FALSE
+}
+
+
 # test will be added later
 test_that("Test something", {
     
